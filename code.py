1a352cea386ef22dd89a1de109b8ec6fe461f09f
lingabo <lingabojunior@gmail.com>
2022-12-01 16:20:40 +0100

    Edit range fonction

--- code.py
+++ code.py
@@ -1,13 +1,13 @@
+
 print("Welcome to the calorie counter\n")
 print("Please enter your calories for the last 7 days")
 
-week = [] #Create a list to store each day's value in.
+week = [] 
 for i in range(7):
-    day = int(input("Day " + str(i+1) + ":")) # Dynamically generate the input using a for loop
-    week.append(day) #add each day to the list
+    day = int(input("Day " + str(i+1) + ":")) 
 
-total = sum(week) #use the handy sum()  function to work out the total :-)
-average = int(total / 7) #We use int again to clean the number up
-
+total = sum(week) 
+average = int(total / 7) 
 print("Your total calorie intake for the week:", total)
 print("Your average calorie intake for the week:", average)
+
